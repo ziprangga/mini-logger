25586fd038cfe5f729ba069e152c4409f71594c5
ziprangga <ziprangga@gmail.com>
2026-03-18 07:15:49 +0700

    test using sample

--- sample/src/main.rs
+++ sample/src/main.rs
@@ -1,9 +1,20 @@
 use mini_logger::*;
 mod try_module;
 
+fn init_using_builder_or_direct(direct: bool) {
+    if direct {
+        mini_logger::init()
+    } else {
+        mini_logger::Builder::new()
+            .env_default()
+            .output_stdout()
+            .init()
+    }
+}
+
 fn main() {
-    // Initialize the global logger
-    mini_logger::init(); // sets defaults from RUST_LOG or Info
+    // // Initialize the global logger
+    init_using_builder_or_direct(true);
 
     // Log messages at different levels
     error!("This is an error");
@@ -11,13 +22,6 @@ fn main() {
     info!("This is info");
     debug!("This is debug");
     trace!("This is trace");
-
-    // //You can also test with the builder
-    // mini_logger::Builder::new()
-    //     .env_default()
-    //     .default_format()
-    //     .output_stdout()
-    //     .init();
 
     info!("Another info after builder init");
 
